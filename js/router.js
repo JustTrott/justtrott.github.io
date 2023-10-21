2c993fd6c835fafc6e8c124944731c606cf61a3f
JustTrott <69697147+JustTrott@users.noreply.github.com>
2023-10-21 18:34:15 +0400

    Move to hash routing

--- js/router.js
+++ js/router.js
@@ -4,8 +4,8 @@
 const route = (event) => {
 	event = event || window.event;
 	event.preventDefault();
-	window.history.pushState({}, "", event.target.href);
-	handleLocation();
+	const path = event.target.getAttribute("href").substr(1); // Remove the leading '#'
+	window.location.hash = path; // Set the hash part of the URL
 };
 
 const routes = {
@@ -13,16 +13,22 @@ const routes = {
 	"/notes": "/pages/notes.html",
 	"/projects": "/pages/projects.html",
 	"/contacts": "/pages/contacts.html",
+	404: "/pages/404.html", // Use an actual 404 page
 };
 
 const handleLocation = async () => {
-	const path = window.location.pathname;
-	const url = routes[path] || routes[404];
+	const hash = window.location.hash.substr(1); // Remove the leading '#'
+	const url = routes[hash] || routes["404"];
 	const html = await fetch(url).then((response) => response.text());
 	document.getElementById("main-container").innerHTML = html;
 };
 
-window.onpopstate = handleLocation;
+// Use the "hashchange" event instead of "popstate"
+window.addEventListener("hashchange", handleLocation);
+
+// Remove this line to avoid unnecessary function call
+// window.onpopstate = handleLocation;
+
 window.route = route;
 
 handleLocation();
