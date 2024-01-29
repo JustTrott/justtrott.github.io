7d13ac2f204c14e4406c37ac104450f407f9efd8
JustTrott <greenvisp@gmail.com>
2024-01-29 15:10:16 +0400

    server check

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ import { route, handleLocation } from "./router.js";
 import { blob } from "./blob.js";
 import { changeSticky } from "./sticky.js";
 
-// // if there is no hash, add one
-// if (window.location.hash == "") {
-// 	window.location.hash = "#/";
-// }
+// if there is no hash, add one
+if (window.location.hash == "") {
+	console.log(window.location);
+}
 
 // Use the "hashchange" event instead of "popstate"
 window.addEventListener("hashchange", handleLocation);
